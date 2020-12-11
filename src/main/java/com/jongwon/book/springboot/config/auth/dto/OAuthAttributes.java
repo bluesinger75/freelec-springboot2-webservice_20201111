576a3d42ce821195e7ae6ddb25df722549643e1e
bluesinger75 <bluesinger75@gmail.com>
2020-12-11 17:11:08 +0900

    네이버 로그인

--- src/main/java/com/jongwon/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jongwon/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,6 +25,9 @@ public class OAuthAttributes {
     }
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -37,6 +40,19 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
     // User 엔티티를 생성
     public User toEntity() {
         return User.builder()
