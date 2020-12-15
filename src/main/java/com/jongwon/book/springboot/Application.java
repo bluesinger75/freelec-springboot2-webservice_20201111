d8c426f2a71975bdbd232e5d50eaf45718861de6
bluesinger75 <bluesinger75@gmail.com>
2020-12-15 15:08:55 +0900

    스프링 테스트 수정

--- src/main/java/com/jongwon/book/springboot/Application.java
+++ src/main/java/com/jongwon/book/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // 메인 클래스
 // @SpringBootApplication 으로 인해 스프링 부트의 자동 설정, 스프링Bean 일기와 생성을 모두 자동으로 설정됩니다.
 // 특히나 @SpringBootApplication 이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야만 합니다.
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing //JPA Auditing 활성화 -> 삭제함
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
